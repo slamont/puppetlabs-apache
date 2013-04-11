ca6bea27f676b7917349cc921070131350a16163
Sylvain Lamontagne <slamontagne@iweb.com>
2013-04-11 13:13:29 -0400

    Fix test for 1.8* version of ruby
    The tests were failing on my computer with ruby 1.8.7 because of the
    comma.

--- spec/classes/apache_spec.rb
+++ spec/classes/apache_spec.rb
@@ -63,7 +63,7 @@ describe 'apache', :type => :class do
     ].each do |modname|
       it { should contain_file("#{modname}.load").with(
         'path'   => "/etc/apache2/mods-available/#{modname}.load",
-        'ensure' => 'file',
+        'ensure' => 'file'
       ) }
       it { should contain_file("#{modname}.load symlink").with(
         'path'   => "/etc/apache2/mods-enabled/#{modname}.load",
@@ -88,7 +88,7 @@ describe 'apache', :type => :class do
     ].each do |modname|
       it { should contain_file("#{modname}.load").with(
         'path'   => "/etc/apache2/mods-available/#{modname}.load",
-        'ensure' => 'file',
+        'ensure' => 'file'
       ) }
       it { should contain_file("#{modname}.load symlink").with(
         'path'   => "/etc/apache2/mods-enabled/#{modname}.load",
@@ -97,7 +97,7 @@ describe 'apache', :type => :class do
       ) }
       it { should contain_file("#{modname}.conf").with(
         'path'   => "/etc/apache2/mods-available/#{modname}.conf",
-        'ensure' => 'file',
+        'ensure' => 'file'
       ) }
       it { should contain_file("#{modname}.conf symlink").with(
         'path'   => "/etc/apache2/mods-enabled/#{modname}.conf",

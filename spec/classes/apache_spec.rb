dbe28c9b5172c06a890eb37f91277c38f964fe88
Ewoud Kohl van Wijngaarden <e.kohlvanwijngaarden@oxilion.nl>
2013-04-12 14:47:28 +0200

    Config files should be owned by root, not apache
    This fixes https://github.com/hunner/puppetlabs-apache/issues/15.

--- spec/classes/apache_spec.rb
+++ spec/classes/apache_spec.rb
@@ -46,11 +46,11 @@ describe 'apache', :type => :class do
       'require' => 'Package[httpd]'
       )
     }
-    it { should contain_concat("/etc/apache2/ports.conf").with(
+    it { should contain_file("/etc/apache2/ports.conf").with(
       'owner'   => 'root',
       'group'   => 'root',
       'mode'    => '0644',
-      'notify'  => 'Service[httpd]',
+      'notify'  => 'Service[apache2]',
       )
     }
     # Assert that load files are placed and symlinked for these mods, but no conf file.
@@ -138,7 +138,7 @@ describe 'apache', :type => :class do
       'require' => 'Package[httpd]'
       )
     }
-    it { should contain_concat("/etc/httpd/conf/ports.conf").with(
+    it { should contain_file("/etc/httpd/conf/ports.conf").with(
       'owner'   => 'root',
       'group'   => 'root',
       'mode'    => '0644',
